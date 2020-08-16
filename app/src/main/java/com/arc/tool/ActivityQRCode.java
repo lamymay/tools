008295d877a0f94f38bccf3c6caddb495e37dc53
lamymay <lamymay@outlook.com>
2020-08-17 00:33:30 +0800

    扫描二维码

--- app/src/main/java/com/arc/tool/ActivityQRCode.java
+++ app/src/main/java/com/arc/tool/ActivityQRCode.java
@@ -1,11 +1,14 @@
 package com.arc.tool;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import com.arc.tool.code.CreateCode;
+import com.arc.tool.code.ScanQRImage;
 
 /**
  * 活动1
@@ -15,60 +18,56 @@ import com.arc.tool.code.CreateCode;
  */
 public class ActivityQRCode extends AppCompatActivity {
 
-//    /**/
-//     * 测试代码
-//     */
-//    private CreateCode createCodeTool = new CreateCode();
+    //  private EditText editText;
+    //    private AutoCompleteTextView textView;
 
-
-    private EditText editText;
-    private TextView textView;
-
+    //输出图片
+//    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
 
-
-        editText = (EditText)findViewById(R.id.qr_input_1);
-        textView = (TextView)findViewById(R.id.outputText);
-
-//设置EditText按键bai输入时的事件
-        editText.setOnKeyListener(new EditText.OnKeyListener(){
-            @Override
-            public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
-// TODO Auto-generated method stub
-                textView.setText(editText.getText());//获取edittext的内容
-                return false;
-            }
-        });
+//        imageView = findViewById(R.id.outputImage);
 
 
         //通过R.layout.文件名称来关联这个
         //点击事件
-        //Button qr_btn_create_img = (Button) findViewById(R.id.qr_btn_create_img);
-        EditText clickEvent = (EditText) findViewById(R.id.qr_input_1);
-
+        Button clickEvent = (Button) findViewById(R.id.qrBtnCreateImg);
+        Button qrBtnScanImageButton = (Button) findViewById(R.id.qrBtnScanImage);
         clickEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //输出图片
-                ImageView imageView = findViewById(R.id.outputImage);
                 //输入框
-                EditText input = (EditText) findViewById(R.id.qr_input_1);
-
+                AutoCompleteTextView input = (AutoCompleteTextView) findViewById(R.id.qrInput1);
                 String inputString = input.getText().toString();
                 System.out.println(input);
                 System.out.println(inputString);
+
+                //输出图片
+                ImageView imageView = findViewById(R.id.qrOutputImage);
                 new CreateCode().createQR(inputString, imageView, getApplicationContext());
             }
         });
 
+        //点击 启动相机识别二维码
+        qrBtnScanImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //输入框
+                AutoCompleteTextView input = (AutoCompleteTextView) findViewById(R.id.qrInput1);
+                String inputString = input.getText().toString();
+                System.out.println(input);
+                System.out.println(inputString);
+
+                //输出图片
+                ImageView imageView = findViewById(R.id.qrOutputImage);
+                new ScanQRImage().scanQRImage(  getApplicationContext());
+            }
+        });
+
     }
-
-
-
 
 
 }
---
+++ app/src/main/java/com/arc/tool/code/ScanQRImage.java
@@ -0,0 +1,18 @@
+package com.arc.tool.code;
+
+
+import android.content.Context;
+
+/**
+ * 扫描二维码
+ *
+ * @author may
+ */
+public class ScanQRImage {
+
+    public void scanQRImage(Context applicationContext) {
+        //todo
+
+
+    }
+}
